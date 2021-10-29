c60b0e4d1e17b974a39091fe869a2f7354fe89b8
Tatsuhiro Ikebe <tatuhiro315@ezweb.ne.jp>
2021-10-29 22:22:30 +0900

    Update/docker rosbag gmapping (#2)
    * Add vscode
    
    * Update docker-rosbag-gmapping

---
+++ rosbag_play/mapping/gmapping/scripts/download_rosbag.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xve
+
+ROSBAG_DIR=~/tsukuba_bag
+FILE_ID=1KuhIFHGd5TuUXpT7oKzMXzhHdzOvfY0v
+FILE_NAME=tsukuba_bag_2021-10-23-14-07.bag
+ROSBAG_FILE_FIND=$(find ~/tsukuba_bag/ -name tsukuba_bag_2021-10-23-14-07.bag)
+ROSBAG_SIZE=$(du -s $ROSBAG_FILE_FIND | awk '{print $1}')
+
+mkdir -p $ROSBAG_DIR 
+cd $ROSBAG_DIR
+
+if [[ $ROSBAG_FILE_FIND != $ROSBAG_DIR/$FILE_NAME ]]; then
+  curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+  CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+  curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o ${FILE_NAME}
+
+  if [ $ROSBAG_SIZE -lt 4000000 ]; then
+    rosbag decompress $FILE_NAME
+    printf '\033[42m%s\033[m\n' 'ROSBAG DOWNLOAD SUCCEED'
+  fi
+elif [[ $ROSBAG_FILE_FIND == $ROSBAG_DIR/$FILE_NAME ]]; then
+  if [ $ROSBAG_SIZE -lt 4000000 ]; then
+    rosbag decompress $ROSBAG_FILE_FIND
+    printf '\033[42m%s\033[m\n' 'ROSBAG DOWNLOAD SUCCEED'
+  else
+    printf '\033[37m%s\033[m\n' 'ALREADY EXIT ROSBAG'
+  fi
+fi
