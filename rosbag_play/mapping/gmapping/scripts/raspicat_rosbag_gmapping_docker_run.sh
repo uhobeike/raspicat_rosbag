9e38ffabcd72709d43211e14be91b7c8c7fb47f6
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-10-29 22:21:46 +0900

    Update docker-rosbag-gmapping

--- rosbag_play/mapping/gmapping/scripts/raspicat_rosbag_gmapping_docker_run.sh
+++ rosbag_play/mapping/gmapping/scripts/raspicat_rosbag_gmapping_docker_run.sh
@@ -3,14 +3,16 @@
 xhost +local:docker && \
 docker run -it --rm --gpus all \
     -v $1:/home/catkin_ws/src/raspicat_slam:rw \
+    -v $HOME/tsukuba_bag/:/home/tsukuba_bag:rw \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e LIBGL_ALWAYS_INDIRECT=1 \
     --privileged \
     --net=host \
-ubeike/raspicat-ros-melodic-rosbag-gmapping /bin/bash -c \
-    "source /home/.bashrc && \
-    ls && \
-    /bin/bash" && \
+    --name raspicat_gmapping \
+ubeike/raspicat-ros-melodic-rosbag-gmapping /bin/bash -i -c \
+    "bash /run_gmapping.sh $3" && \
 xhost -local:docker
+
+killall -9 rosmaster
