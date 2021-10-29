bb9027ce226bf617cc91c6c0761a43865ccf3074
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-10-29 23:25:09 +0900

    Fix

--- rosbag_play/mapping/gmapping/scripts/run_gmapping.sh
+++ rosbag_play/mapping/gmapping/scripts/run_gmapping.sh
@@ -4,4 +4,3 @@ source /root/.bashrc
 rosparam set /use_sim_time true
 rosbag play $(find /home -name *.bag | awk 'NR==2') --clock --topics /tf /tf_static /scan /odom -r $1&
 roslaunch raspicat_slam slam_remote_pc.launch
-# docker exec -i -t $(docker ps -a | awk '$0=$1' | awk 'NR==2')
