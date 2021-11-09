312e8161868cdf73a1ea9485f8e55d988134949e
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-11-09 14:17:32 +0900

    Add comment

--- rosbag_play/mapping/gmapping/scripts/run_gmapping.sh
+++ rosbag_play/mapping/gmapping/scripts/run_gmapping.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# This file is copied in the docker.
 source /root/.bashrc
 rosparam set /use_sim_time true
 rosbag play $(find /home -name $1) --clock --topics /tf /tf_static /scan /odom -r $2&
