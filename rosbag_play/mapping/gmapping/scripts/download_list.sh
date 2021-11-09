bc89a7daf001d3c36c847b770db2ae5df079c088
RikuUchida <s18c1017se@s.chibakoudai.jp>
2021-11-09 17:13:58 +0900

    Add rosbag list

--- rosbag_play/mapping/gmapping/scripts/download_list.sh
+++ rosbag_play/mapping/gmapping/scripts/download_list.sh
@@ -8,4 +8,16 @@ FILE_NAME[0]=tsukuba_bag_2021-10-23-14-07.bag
 #https://drive.google.com/file/d/12hGoy-V2c2cW0fnbkyJ0uoJr34EU3dVI/view?usp=sharing
 FILE_ID[1]=12hGoy-V2c2cW0fnbkyJ0uoJr34EU3dVI
 FILE_NAME[1]=tsudanumna_raspionly_2021-11-04-19-24-12.bag
+
+#https://drive.google.com/file/d/1KiqICkS_PgOc7FB7TNF1-x4pwh7CZWw5/view?usp=sharing
+FILE_ID[2]=1KiqICkS_PgOc7FB7TNF1-x4pwh7CZWw5
+FILE_NAME[2]=2021-11-07-11-many_observers.bag
+
+#https://drive.google.com/file/d/13p2o1gFCb6oQqIpAeS-8_16vk4uuCT69/view?usp=sharing
+FILE_ID[3]=13p2o1gFCb6oQqIpAeS-8_16vk4uuCT69
+FILE_NAME[3]=2021-11-07-11-good_mapping.bag
+
+#https://drive.google.com/file/d/1OiXY3UqdZlg3iOH_a4jgrFsShb739RhA/view?usp=sharing
+FILE_ID[4]=1OiXY3UqdZlg3iOH_a4jgrFsShb739RhA
+FILE_NAME[4]=2021-11-07-15-following_main_course.bag
 #################################################
