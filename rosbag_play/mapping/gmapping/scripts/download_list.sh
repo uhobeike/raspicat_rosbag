f4e62d8eda30eec71423dcdd17bceb2814927289
Tatsuhiro Ikebe <tatuhirobeike@gmail.com>
2021-11-10 14:48:54 +0900

    Update download_list.sh
    Fix FILE_NAME

--- rosbag_play/mapping/gmapping/scripts/download_list.sh
+++ rosbag_play/mapping/gmapping/scripts/download_list.sh
@@ -11,13 +11,13 @@ FILE_NAME[1]=tsudanumna_raspionly_2021-11-04-19-24-12.bag
 
 #https://drive.google.com/file/d/1KiqICkS_PgOc7FB7TNF1-x4pwh7CZWw5/view?usp=sharing
 FILE_ID[2]=1KiqICkS_PgOc7FB7TNF1-x4pwh7CZWw5
-FILE_NAME[2]=2021-11-07-11-many_observers.bag
+FILE_NAME[2]=2021-11-07-11_many_observers.bag
 
 #https://drive.google.com/file/d/13p2o1gFCb6oQqIpAeS-8_16vk4uuCT69/view?usp=sharing
 FILE_ID[3]=13p2o1gFCb6oQqIpAeS-8_16vk4uuCT69
-FILE_NAME[3]=2021-11-07-11-good_mapping.bag
+FILE_NAME[3]=2021-11-07-11_good_mapping.bag
 
 #https://drive.google.com/file/d/1OiXY3UqdZlg3iOH_a4jgrFsShb739RhA/view?usp=sharing
 FILE_ID[4]=1OiXY3UqdZlg3iOH_a4jgrFsShb739RhA
-FILE_NAME[4]=2021-11-07-15-following_main_course.bag
+FILE_NAME[4]=2021-11-07-15_following_main_course.bag
 #################################################
