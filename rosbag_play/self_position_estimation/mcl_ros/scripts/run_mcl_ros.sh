6c051d2e35d1c8a15aa04154d31267c0fded717f
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-11-12 01:16:48 +0900

    Fix odom tf

--- rosbag_play/self_position_estimation/mcl_ros/scripts/run_mcl_ros.sh
+++ rosbag_play/self_position_estimation/mcl_ros/scripts/run_mcl_ros.sh
@@ -6,7 +6,6 @@ source /root/.bashrc
 rosparam set /use_sim_time true
 rosrun map_server map_server $(find /home -name $1)&
 rosrun rviz rviz -d $(find /home -name $4)&
-rosrun tf static_transform_publisher 0 0 0 0 0 0 /map /odom 100&
 rosrun tf static_transform_publisher 0.144 0.0 0.04 0 0 0 /base_link /urg_lrf_link 100&
 rosbag play $(find /home -name $2) --clock --topics /tf /tf_static /scan /odom -r $3&
 roslaunch raspicat_navigation mcl_ros.launch
