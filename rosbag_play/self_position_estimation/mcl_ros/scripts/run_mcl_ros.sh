973c195debfd45495e098e32c267e3554e4d9de8
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-11-11 18:47:52 +0900

    Fature rosbag_play_mcl_ros

---
+++ rosbag_play/self_position_estimation/mcl_ros/scripts/run_mcl_ros.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# This file is copied in the docker.
+# The argument $1 $2 is the argument of "bash /run_gmapping.sh ${FILE_NAME [$4]} $5".
+source /root/.bashrc
+rosparam set /use_sim_time true
+rosrun map_server map_server $(find /home -name $1)&
+rosrun rviz rviz -d $(find /home -name $4)&
+rosrun tf static_transform_publisher 0 0 0 0 0 0 /map /odom 100&
+rosrun tf static_transform_publisher 0.144 0.0 0.04 0 0 0 /base_link /urg_lrf_link 100&
+rosbag play $(find /home -name $2) --clock --topics /tf /tf_static /scan /odom -r $3&
+roslaunch raspicat_navigation mcl_ros.launch
