833f6f9f98c874c78d7e657ddabb38dc0d7291f7
Tatsuhiro Ikebe <tatuhiro315@ezweb.ne.jp>
2021-11-12 02:03:50 +0900

    Fix/emcl and mcl ros download url (#30)
    * Fix ci.yaml
    
    * Fix emcl and mcl_ros download-url

--- helper_scripts/environment_setting.sh
+++ helper_scripts/environment_setting.sh
@@ -2,10 +2,14 @@
 
 sudo apt install -y python-catkin-tools
 
-mkdir -p ~/raspicat_rosbagslam_ws/src && cd ~/raspicat_rosbagslam_ws/src 
+mkdir -p ~/raspicat_rosbagslam_ws/src && cd ~/raspicat_rosbagslam_ws/src
+
 git clone https://github.com/CIT-Autonomous-Robot-Lab/raspicat_slam.git
 git clone https://github.com/CIT-Autonomous-Robot-Lab/raspicat_navigation.git
+git clone https://github.com/CIT-Autonomous-Robot-Lab/emcl.git
+svn export https://github.com/CIT-Autonomous-Robot-Lab/mcl_ros/trunk/src/mcl_ros
 git clone https://github.com/uhobeike/raspicat_rosbag.git
+
 rosdep install -r -y --from-paths --ignore-src .
 cd ~/raspicat_rosbagslam_ws/
 catkin build
