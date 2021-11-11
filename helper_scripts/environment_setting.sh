ccf9c877e65cad5d86d280f54d28956dcb309fa5
Tatsuhiro Ikebe <tatuhiro315@ezweb.ne.jp>
2021-11-12 01:41:29 +0900

    Update/readme.md 4 (#29)
    * Fix
    
    * Update README.md

--- helper_scripts/environment_setting.sh
+++ helper_scripts/environment_setting.sh
@@ -4,8 +4,9 @@ sudo apt install -y python-catkin-tools
 
 mkdir -p ~/raspicat_rosbagslam_ws/src && cd ~/raspicat_rosbagslam_ws/src 
 git clone https://github.com/CIT-Autonomous-Robot-Lab/raspicat_slam.git
-git clone https://github.com/uhobeike/raspicat_navigation.git
+git clone https://github.com/CIT-Autonomous-Robot-Lab/raspicat_navigation.git
 git clone https://github.com/uhobeike/raspicat_rosbag.git
+rosdep install -r -y --from-paths --ignore-src .
 cd ~/raspicat_rosbagslam_ws/
 catkin build
 source ~/raspicat_rosbagslam_ws/devel/setup.bash
