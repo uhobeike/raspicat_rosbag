58caa762ae3e3264e8cdc014e8749c93be708e16
uhobeike <beike315@icloud.com>
2022-02-02 11:59:57 +0900

    Fix

--- rosbag_play/mapping/gmapping/scripts/run_gmapping_exec.sh
+++ rosbag_play/mapping/gmapping/scripts/run_gmapping_exec.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
 # This file is copied in the docker.
-# ex) . run_gmapping.sh 3 1
+# ex) bash run_gmapping.sh 3 1
 source /root/.bashrc
 . download_list.sh
-rosocre |&
-wait
+roscore&
+sleep 1
 rosparam set /use_sim_time true
-rosbag play $(find /home -name ${FILE_NAME[$1]}) --clock --topics /tf /tf_static /scan /odom -r $2 |&
+rosbag play $(find /home -name ${FILE_NAME[$1]}) --clock --topics /tf /tf_static /scan /odom -r $2&
 roslaunch raspicat_slam slam_remote_pc.launch
+killall -9 rosmaster
